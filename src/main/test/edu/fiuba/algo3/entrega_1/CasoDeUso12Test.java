07e06c48fdc2ea16c2510cc599132ea76dab5de4
RDJulian <jruizd@fi.uba.ar>
2022-11-11 14:09:35 -0300

    fix: Caso de uso 12 ahora prueba que la vida no deberia subir al pasar turnos.

--- src/main/test/edu/fiuba/algo3/entrega_1/CasoDeUso12Test.java
+++ src/main/test/edu/fiuba/algo3/entrega_1/CasoDeUso12Test.java
@@ -3,7 +3,6 @@ package edu.fiuba.algo3.entrega_1;
 import edu.fiuba.algo3.modelo.Estructura.Acceso;
 import edu.fiuba.algo3.modelo.Estructura.Estructura;
 import edu.fiuba.algo3.modelo.Estructura.Pilon;
-import edu.fiuba.algo3.modelo.Estructura.PuertoEstelar;
 import edu.fiuba.algo3.modelo.Posicion.Posicion;
 import org.junit.jupiter.api.Test;
 
@@ -12,23 +11,25 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class CasoDeUso12Test {
     @Test
     public void test01DaniarUnaEstructuraProtossHastaQuitarleVidaDeberiaRegenerarSoloSuEscudoAlPasarTurnos() {
-        Estructura estructura = new Acceso(new Posicion(0,0));
-        Pilon pilon = new Pilon(new Posicion(1,1));
+        Estructura estructura = new Acceso(new Posicion(0, 0));
+        Pilon pilon = new Pilon(new Posicion(1, 1));
         pasarKTurnos(pilon, 5);
         estructura.construible(pilon);
 
-        pasarKTurnos(estructura,8);
+        pasarKTurnos(estructura, 8);
         estructura.daniar(555);
 
         assertEquals(estructura.getVida(), 445);
         assertEquals(estructura.getDefensa(), 0);
 
-        pasarKTurnos(estructura,49);
+        pasarKTurnos(estructura, 49);
 
+        assertEquals(estructura.getVida(), 445);
         assertEquals(estructura.getDefensa(), 490);
 
-        pasarKTurnos(estructura,3);
+        pasarKTurnos(estructura, 3);
 
+        assertEquals(estructura.getVida(), 445);
         assertEquals(estructura.getDefensa(), 500);
     }
 
